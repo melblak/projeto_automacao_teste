4ebd7e0acddb79e1521b1fc4bd8751f5b03fe88e
unknown <melinda.blak@gmail.com>
2026-01-08 13:17:12 -0300

    Primeira versão das funções de execução

--- placa.cpp
+++ placa.cpp
@@ -14,13 +14,16 @@ placa::~placa() {
     desconectar();
 }
 
+
+//Função para verificar os dados da placa e chamar a conexão
 void placa::verifica_porta_e_conecta()
 {
     foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
         if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
             if(serialPortInfo.vendorIdentifier() == id_fornecedor){
                 if(serialPortInfo.productIdentifier() == id_produto){
-                    conectar(serialPortInfo.portName());
+                    QObject::connect(this,&placa::portaVerificada,this,&placa::conectar);
+                    emit(portaVerificada(serialPortInfo.portName()));
                 }
             }
         }
@@ -28,7 +31,7 @@ void placa::verifica_porta_e_conecta()
 }
 
 //Funções de inicialização da placa
-void placa::conectar(const QString &portaNome)
+void placa::conectar(QString portaNome)
 {
     arduino->setPortName(portaNome);
     arduino->open(QIODevice::ReadWrite);
@@ -38,10 +41,12 @@ void placa::conectar(const QString &portaNome)
     arduino->setStopBits(QSerialPort::OneStop);
     arduino->setFlowControl(QSerialPort::NoFlowControl);
 
+    QObject::connect(this,&placa::placaConectada,this,&placa::inicia_testemunho);
+    emit(placaConectada());
     qDebug() << "Inicialização completa";
 }
 
-//Função para finalização da placa
+// Função para finalização da placa
 void placa::desconectar()
 {
     if(arduino->isOpen()){
@@ -49,6 +54,33 @@ void placa::desconectar()
     }
 }
 
+// Função que comunica o arduino para acionar o motor
+void placa::altera_passo_rotacao()
+{
+    arduino->write("R");
+}
+
+// Função que faz o loop que ajusta a posição, tira foto e rotaciona
+void placa::executar()
+{
+    for(int i=1; i<=quant_ajustes; i++){
+        for(int j=0; j<=passos_por_rotacao; j+=largura_passo){
+            tira_foto();
+            altera_passo_rotacao();
+        }
+        altera_passo_ajuste();
+    }
+}
+
+// Função de trigger da execução
+void placa::inicia_testemunho()
+{
+    // Implementar trigger para o processo iniciar
+    QObject::connect(this,&placa::execucaoIniciada,this,&placa::executar);
+    emit(execucaoIniciada());
+}
+
 /* Para mover o motor:
  * 1)Criar métodos públicos de trigger para enviar dados para a entrada serial
  * 2)Criar código no ArduinoIDE para receber o dado e mover o motor de acordo
+*/
--- placa.h
+++ placa.h
@@ -13,14 +13,26 @@ public:
     ~placa();
 
     void verifica_porta_e_conecta();
-    void conectar(const QString &portaNome);
     void desconectar();
-
+    void tira_foto();
+    void altera_passo_rotacao();
+    void altera_passo_ajuste();
 private:
     QSerialPort *arduino;
+    QString nome_da_porta;
     static const quint16 id_fornecedor; // Atribuir valores fixos ao conectar o arduino
     static const quint16 id_produto;
+    static const int passos_por_rotacao; // Quantidade de passos em uma rotação
+    static const int largura_passo; // Quantidade de passos a serem feitos para cada foto
+    static const int quant_ajustes; // Quantidade de posições a serem fotografadas
+public slots:
+    void conectar(QString portaNome);
+    void executar();
+    void inicia_testemunho();
 signals:
+    void portaVerificada(QString portaNome);
+    void placaConectada();
+    void execucaoIniciada();
 };
 
 #endif // PLACA_H
