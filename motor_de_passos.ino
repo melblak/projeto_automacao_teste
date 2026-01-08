54618576c3f32de9c4b29d9563981e6974321301
Lais Nazareth <lais.nazareth@gmail.com>
2026-01-08 14:30:58 -0300

    Add files via upload
    feat: código .ino do motor de passos

---
+++ motor_de_passos.ino
@@ -0,0 +1,42 @@
+#include <Stepper.h>
+
+// Constantes arbitrárias (podem ser alteradas quando for efetuado o teste físico)
+const int stepPin = 4;
+const int dirPin = 5;
+
+const int delay = 800;
+
+const int passosParaMover = 400
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  digitalWrite(dirPin, HIGH);
+
+  Serial.println("Arduino pronto para rotação");
+
+}
+
+void loop() {
+  if (Serial.available() > 0){
+    char comando = Serial.read();
+
+    if (comando = 'R') {
+      moverMotor(passosParaMover);
+
+      // avisar ao Qt que o movimento acabou com o sinal 'K' (OK)
+      // TO-DO: implementar esse 'handhsake' no codigo qt
+      Serial.print('K');
+    }
+  }
+}
+
+void moverMotor(int passos){
+  for (int i=0; i < passos; i++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(800);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(800);
+  }
+}
