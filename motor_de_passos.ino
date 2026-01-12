d5418c8d26d9b2c45f24e8049b293997a8307b19
unknown <melinda.blak@gmail.com>
2026-01-12 10:17:53 -0300

    Adicionando arquivo do motor antes do merge

---
+++ motor_de_passos.ino
@@ -0,0 +1,38 @@
+const int stepPin = 4;
+const int dirPin = 5;
+
+const int microDelay = 800;
+
+const int passosParaMover = 40
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  digitalWrite(dirPin, HIGH);
+
+  Serial.println("Arduino pronto para rotação");
+
+}
+
+void loop() {
+  if (Serial.available() > 0){
+    char comando = Serial.read();
+
+    if (comando == 'R') {
+      moverMotor(passosParaMover);
+
+      // avisar ao Qt que o movimento acabou com o sinal 'P'
+      Serial.print('P');
+    }
+  }
+}
+
+void moverMotor(int passos){
+  for (int i=0; i < passos; i++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(microDelay);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(microDelay);
+  }
+}
